7fbbe78a749cf05eeaa2bf05950be84d263e03a6
Jorge Brandon Nunez <jjbnunez97@gmail.com>
2021-03-25 15:54:14 -0400

    Fix zsh install commands

--- install.sh
+++ install.sh
@@ -22,5 +22,7 @@ ln -s "$HOME/dotfiles/i3" "$HOME/.config"
 #######
 # zsh #
 #######
+mkdir -p "$HOME/.config/zsh"
 ln -sf "$HOME/dotfiles/zsh/.zshenv" "$HOME"
 ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.config/zsh"
+ln -sf "$HOME/dotfiles/zsh/aliases" "$HOME/.config/zsh/aliases"
---
+++ zsh/.zshrc
@@ -0,0 +1,2 @@
+# We must source our aliases
+source "$XDG_CONFIG_HOME/zsh/aliases"
