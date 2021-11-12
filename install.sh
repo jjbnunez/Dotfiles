f182ec295527c8b85d4def148aee9e5e6db40917
Jorge Brandon Nunez <jjbnunez97@gmail.com>
2021-11-11 23:31:55 -0500

    Huge progress

--- install.sh
+++ install.sh
@@ -19,6 +19,12 @@ ln -s "$DOTFILES/X11" "$XDG_CONFIG_HOME"
 rm -rf "$XDG_CONFIG_HOME/i3"
 ln -s "$DOTFILES/i3" "$XDG_CONFIG_HOME"
 
+#########
+# dunst #
+#########
+mkdir -p "$XDG_CONFIG_HOME/dunst"
+ln -sf "$DOTFILES/dunst/dunstrc" "$XDG_CONFIG_HOME/dunst/dunstrc"
+
 #######
 # zsh #
 #######
@@ -28,3 +34,14 @@ ln -sf "$DOTFILES/zsh/.zshrc" "$XDG_CONFIG_HOME/zsh"
 ln -sf "$DOTFILES/zsh/aliases" "$XDG_CONFIG_HOME/zsh/aliases"
 rm -rf "$XDG_CONFIG_HOME/zsh/external"
 ln -sf "$DOTFILES/zsh/external" "$XDG_CONFIG_HOME/zsh"
+
+#########
+# fonts #
+#########
+mkdir -p "$XDG_DATA_HOME"
+cp -rf "$DOTFILES/fonts" "$XDG_DATA_HOME"
+
+#######
+# git #
+#######
+ln -sf "$DOTFILES/git/.gitconfig" "$HOME"
