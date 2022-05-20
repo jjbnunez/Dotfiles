76b890ad2ed69aacd2daaa0fc3563bf79fa9e17a
JJ Nuñez <jj@votepalmbeach.gov>
2022-05-20 17:50:01 -0400

    Fix broken main bucket problem in scoop

--- install.ps1
+++ install.ps1
@@ -31,18 +31,20 @@ try {
     # scoop #
     #########
 
-    $scoopIsInstalled = ($null -ne (Get-Command scoop -ErrorAction Ignore))
-    if (-not $scoopIsInstalled) {
+    $isScoopInstalled = ($null -ne (Get-Command scoop -ErrorAction Ignore))
+    if (-not $isScoopInstalled) {
         # install scoop
         Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
         
         # 7zip and git (NEEDED FOR BUCKET MANAGEMENT)
-	scoop install 7zip
+    	scoop install 7zip
         scoop install git
         git config --system --unset credential.helper
         
-	# add extras bucket
-	scoop bucket add extras
+    	# add extras bucket
+    	scoop bucket rm main
+    	scoop bucket add main
+    	scoop bucket add extras
 
         # oh-my-posh
         scoop install oh-my-posh
@@ -60,8 +62,8 @@ try {
         pip install --user pynvim
         npm install --global neovim
 
-	# vscode
-	scoop install vscode
+    	# vscode
+    	scoop install vscode
 
         # scoop updates
         scoop update
