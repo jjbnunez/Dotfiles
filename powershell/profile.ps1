d6a0343008c876201d04d8b0e2a4c56cd218eb8f
JJ Nunez <jj@votepalmbeach.gov>
2023-01-12 15:59:57 -0500

    Change pwsh theme

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,3 +1,3 @@
 $DOTFILES = "$env:DOTFILES"
 $XDG_CONFIG_HOME = "$env:XDG_CONFIG_HOME"
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/agnosterplus.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/json.omp.json" | Invoke-Expression
