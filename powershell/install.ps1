ad6dd17ca97c58ab2b6c626773ca0d97fd178de4
JJ Nunez <jj@votepalmbeach.gov>
2023-01-13 17:37:31 -0500

    Add profile for vscode pwsh extension terminal

--- powershell/install.ps1
+++ powershell/install.ps1
@@ -18,8 +18,10 @@ if (-not $isScoopInstalled) {
 Write-Host "Updating PowerShell profile... " -NoNewLine
 $profileSource = "$env:DOTFILES\powershell\profile.ps1"
 $profileTarget = "$env:USERPROFILE\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
+$profileTargetVSCode = "$env:USERPROFILE\Documents\WindowsPowerShell\Microsoft.VSCode_profile.ps1"
 
 #The New-Item call below looks redundant but it forces subdirectory creation whereas Copy-Item doesn't
 New-Item -Force -ItemType File -Path "$profileTarget" | Out-Null 
 Copy-Item -Path "$profileSource" -Destination "$profileTarget" -Force | Out-Null
+Copy-Item -Path "$profileSource" -Destination "$profileTargetVSCode" -Force | Out-Null
 Write-Host "OK"
