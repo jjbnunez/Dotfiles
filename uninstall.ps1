720269c02f368bd70791d5846e3bb72f0e73984b
JJ Nunez <jjbnunez97@gmail.com>
2023-08-01 17:17:57 -0400

    Create separate envvars uninstall script

--- uninstall.ps1
+++ uninstall.ps1
@@ -7,17 +7,7 @@
  ##
 
 try {
-
-    ############
-    # env vars #
-    ############
-
-    Write-Host "Removing env vars..." -NoNewLine
-    [Environment]::SetEnvironmentVariable("DOTFILES", $null, "User")
-    [Environment]::SetEnvironmentVariable("XDG_CONFIG_HOME", $null, "User")
-    [Environment]::SetEnvironmentVariable("EDITOR", $null, "User")
-    Write-Host "OK"
-
+    . .\envvars\uninstall.ps1
 
     #########
     # scoop #
