d6f655f13e3fb83665faa960df6b36ad3638884d
JJ Nunez <jj@votepalmbeach.gov>
2023-08-03 09:01:31 -0400

    Add helpful script and add vscode host profile

--- powershell/install.ps1
+++ powershell/install.ps1
@@ -17,11 +17,18 @@ if (-not $isScoopInstalled) {
 
 Write-Host "Updating PowerShell profile... " -NoNewLine
 $profileSource = "$env:DOTFILES\powershell\profile.ps1"
-$profileTarget = "$env:USERPROFILE\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
-$profileTargetVSCode = "$env:USERPROFILE\Documents\WindowsPowerShell\Microsoft.VSCode_profile.ps1"
+$profileTarget51 = "$env:USERPROFILE\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
+$profileTargetVSCode51 = "$env:USERPROFILE\Documents\WindowsPowerShell\Microsoft.VSCode_profile.ps1"
+$profileTarget7 = "$env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
+$profileTargetVSCode7 = "$env:USERPROFILE\Documents\PowerShell\Microsoft.VSCode_profile.ps1"
 
 #The New-Item call below looks redundant but it forces subdirectory creation whereas Copy-Item doesn't
-New-Item -Force -ItemType File -Path "$profileTarget" | Out-Null 
-Copy-Item -Path "$profileSource" -Destination "$profileTarget" -Force | Out-Null
-Copy-Item -Path "$profileSource" -Destination "$profileTargetVSCode" -Force | Out-Null
+New-Item -Force -ItemType File -Path "$profileTarget51" 
+New-Item -Force -ItemType File -Path "$profileTarget7" 
+New-Item -Force -ItemType File -Path "$profileTargetVSCode51" 
+New-Item -Force -ItemType File -Path "$profileTargetVSCode7" 
+Copy-Item -Path "$profileSource" -Destination "$profileTarget51" -Force
+Copy-Item -Path "$profileSource" -Destination "$profileTarget7" -Force
+Copy-Item -Path "$profileSource" -Destination "$profileTargetVSCode51" -Force
+Copy-Item -Path "$profileSource" -Destination "$profileTargetVSCode7" -Force
 Write-Host "OK"
