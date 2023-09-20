da5734a8725b4903ba4ff5089975246ebfcb83c9
JJ Nunez <jjbnunez97@gmail.com>
2023-09-20 15:36:05 -0400

    Change omp theme to multiverse neon

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,4 +1,4 @@
 $DOTFILES = "$env:DOTFILES"
 $XDG_CONFIG_HOME = "$env:XDG_CONFIG_HOME"
 Set-PSReadLineKeyHandler -Chord Ctrl+d -Function DeleteCharOrExit
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/json.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/multiverse-neon.omp.json" | Invoke-Expression
