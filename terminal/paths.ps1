e939e3958de2a4096ac6ff734533aefd1c706384
JJ Nunez <jj@votepalmbeach.gov>
2023-09-28 17:44:05 -0400

    Make debug statements for winterm settings paths

--- terminal/paths.ps1
+++ terminal/paths.ps1
@@ -1,2 +1,4 @@
 $There = "$env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
 $Here = "$env:UserProfile\Dotfiles\terminal\settings.json"
+Write-Debug -Message "LocalAppData Windows Terminal settings: '$There'"
+Write-Debug -Message "Dotfiles Windows Terminal settings: '$Here'"
