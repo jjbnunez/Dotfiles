2788ffda46837f8d0d48cdab5c2e913b88f99a2f
JJ Nunez <jjbnunez97@gmail.com>
2024-04-27 15:36:11 -0400

    Set up terminal install and uninstall scripts via winget

---
+++ terminal/uninstall.ps1
@@ -0,0 +1,6 @@
+if ((Get-CimInstance Win32_OperatingSystem).Caption.Contains("Windows 11")) {
+	Import-Module -Name $PSScriptRoot\..\shared\MyHelperModule
+	Uninstall-FromWinGet -Id "Microsoft.WindowsTerminal"
+} else {
+	Write-Warning "You are running Windows 11. The Windows Terminal comes installed by default, so this script will not uninstall it."
+}
