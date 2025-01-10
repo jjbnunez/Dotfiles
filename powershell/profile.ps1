98ff10f48e9958f687e3bcc3600df1e71d4e17e4
JJ Nunez <jjbnunez97@gmail.com>
2025-01-10 13:08:07 -0500

    Change pwsh theme

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,4 +1,4 @@
 Set-PSReadLineKeyHandler -Chord Ctrl+d -Function DeleteCharOrExit
 if ($null -ne (Get-Command oh-my-posh -ErrorAction Ignore)) {
-	oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/spaceship.omp.json" | Invoke-Expression
+	oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/star.omp.json" | Invoke-Expression
 }
