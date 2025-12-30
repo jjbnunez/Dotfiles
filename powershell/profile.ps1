581372886c8dc3dced5861111d5a2767f971b48c
JJ Nunez <jjbnunez97@gmail.com>
2025-12-30 09:23:22 -0500

    Remove unwanted module

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -21,6 +21,3 @@ Register-ArgumentCompleter -Native -CommandName az -ScriptBlock {
 }
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 #f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
-
-Import-Module -Name Microsoft.WinGet.CommandNotFound
-#f45873b3-b655-43a6-b217-97c00aa0db58
