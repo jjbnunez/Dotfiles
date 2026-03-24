a96e8b6a2758a82bb88fdf799198e2fbc9c6553e
JJ Nunez <jjbnunez97@gmail.com>
2026-03-24 11:25:14 -0400

    Clean up old envvars and add new ones

--- envvars/set.ps1
+++ envvars/set.ps1
@@ -1,13 +1,11 @@
 Write-Host "Attempting to set environment variables..."
 
 # Set the environment variables
+[Environment]::SetEnvironmentVariable("HOME", "$env:USERPROFILE", "User")
 [Environment]::SetEnvironmentVariable("DOTFILES", "$env:USERPROFILE\Dotfiles", "User")
-[Environment]::SetEnvironmentVariable("XDG_CONFIG_HOME", "$env:LOCALAPPDATA", "User")
-#[Environment]::SetEnvironmentVariable("EDITOR", "nvim", "User")
 
 # Read the environment variables in for the current session
+$env:HOME = [Environment]::GetEnvironmentVariable("HOME", "User")
 $env:DOTFILES = [Environment]::GetEnvironmentVariable("DOTFILES", "User")
-$env:XDG_CONFIG_HOME = [Environment]::GetEnvironmentVariable("XDG_CONFIG_HOME", "User")
-#$env:EDITOR = [Environment]::GetEnvironmentVariable("EDITOR", "User")
 
 Write-Host "OK"
--- envvars/unset.ps1
+++ envvars/unset.ps1
@@ -1,13 +1,11 @@
 Write-Host "Attempting to unset environment variables..."
 
 # Unset the environment variables
+[Environment]::SetEnvironmentVariable("HOME", $null, "User")
 [Environment]::SetEnvironmentVariable("DOTFILES", $null, "User")
-[Environment]::SetEnvironmentVariable("XDG_CONFIG_HOME", $null, "User")
-#[Environment]::SetEnvironmentVariable("EDITOR", $null, "User")
 
 # Read the environment variables in for the current session
+$env:HOME = $null
 $env:DOTFILES = $null
-$env:XDG_CONFIG_HOME = $null
-#$env:EDITOR = $null
 
 Write-Host "OK"
