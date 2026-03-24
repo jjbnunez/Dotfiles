f2f455a7997a70f06bfead6e1b2b497d2e55d7d2
JJ Nunez <jjbnunez97@gmail.com>
2026-03-24 10:34:25 -0400

    Remove nvim since I primarily use vscode instead

--- nvim/_uninstall.ps1
+++
@@ -1,8 +0,0 @@
-Write-Host "Removing nvim config and plugin manager..." -NoNewLine
-if (Test-Path "$env:LOCALAPPDATA\nvim") {
-	Remove-Item -Recurse -Force -Path "$env:LOCALAPPDATA\nvim"
-}
-if (Test-Path "$env:LOCALAPPDATA\nvim-data") {
-	Remove-Item -Recurse -Force -Path "$env:LOCALAPPDATA\nvim-data"
-}
-Write-Host "OK"
